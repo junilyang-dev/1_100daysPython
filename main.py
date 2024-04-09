064e4a4241ab3dcf256f5f4df9b20450ceb476be
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-04-09 11:42:05 +0000

    1_100daysPython

---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+print("Hello Replit")
+print(' new line print')
+print("""
+  several lines
+      of text
+    print
+    """)#mulitple lines of text
+print('''hello
+  again''')#multiple print statements
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+print("Arum")
+print("Feb 22th 2024")
+print(datetime.today().strftime("Today is %A, %B %d, %Y"))
+print(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))#시간 다름 미쿡?
+print('''I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!''')
+print("I am feeling 😃")
+print("You can follow my progress at replit.com/@Arum")

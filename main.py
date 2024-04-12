d71119ce2090a8f5ba7fa8bb2bacda78b4f8ac5e
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-04-12 12:33:06 +0000

    2_100daysPython

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
-from datetime import datetime
+myName = input("What's you name?: ")
+myAge = input("How old are you? :")
+print("Gee, that's REALLY OLD")
+replit = input("Do you like Replit?")
+print("OF COURSE YOU DO!")
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(myGrandma)
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print(f"myLunch will be {myLunch}")
+print("as soon as possible!")
+print("Definitely not a Phishing Scam")
+print()
+myName = input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+print("Getting to know you!")
 
-print("Hello Replit")
-print(' new line print')
-print("""
-  several lines
-      of text
-    print
-    """)#mulitple lines of text
-print('''hello
-  again''')#multiple print statements
-print("""Anything that starts
-with three quotes, and ends
-in three quotes can span
-many lines and even contain " symbols
-within it without freaking anything out!""")
-print("= MUSIC+ =")
-print("> Songs")
-print("> Albums")
-print("> Artists")
-print("Arum")
-print("Feb 22th 2024")
-print(datetime.today().strftime("Today is %A, %B %d, %Y"))
-print(datetime.today().strftime("%Y/%m/%d %H:%M:%S"))#시간 다름 미쿡?
-print('''I am signing up for Replit's 100 days of Python challenge!
-I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!''')
-print("I am feeling 😃")
-print("You can follow my progress at replit.com/@Junil")
+YourName = input("What is your name?")
+Hungry = input ("What is your favorite food?")
+Music = input("What is your favorite music?")
+WhereAreYou = input("Where are you?")
+
+print("You are")
+print(YourName)
+print() 
+
+print("You're probably hungry for")
+print(Hungry)
+print()
+print("You're probably listening to")
+print (Music)
+print()
+print ("You're probably living in the amazing")
+print (WhereAreYou)
+print() 
+print ("Have a great day!")

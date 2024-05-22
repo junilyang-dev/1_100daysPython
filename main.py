4729966d49717c3248e180850acc0cc3a13aaba5
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-05-22 13:20:33 +0000

    스크랩페이지 스킬 포함

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 all_jobs = []
 
-def scrape_page(url):
+def scrape_page(url,arr):
   response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
   soup = BeautifulSoup(response.content, "html.parser",)
   jobs = soup.find("ul", class_="jobs-list-items").find_all("li")[0:]
@@ -20,7 +20,7 @@ def scrape_page(url):
       'desc': desc,
       'url': url
     }
-    all_jobs.append(job_data)
+    arr.append(job_data)
 
 def get_pages(url):
   response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
@@ -31,7 +31,32 @@ total_pages = get_pages("https://berlinstartupjobs.com/engineering")
 
 for x in range(total_pages-1):
   url = f"https://berlinstartupjobs.com/engineering/page/{x+1}"
-  scrape_page(url)
+  scrape_page(url,all_jobs)
 
 
 print(len(all_jobs))
+
+all_skill_href = []
+
+def get_skill(url):
+  response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
+  soup = BeautifulSoup(response.content, "html.parser",)
+  links_box = soup.find("div", class_="popular_skills").find("ul", class_="links-box").find_all("li", class_="link")[0:]
+  for links in links_box:
+    link = links.find("a")["href"]
+    all_skill_href.append(link)
+
+get_skill("https://berlinstartupjobs.com/engineering")
+
+
+all_jobs_skill = []
+
+def scrape_page_skill(url):
+  response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
+  soup = BeautifulSoup(response.content, "html.parser",)
+  jobs = soup.find("ul", class_="jobs-list-items").find_all("li")[0:]
+
+for skill_href in all_skill_href :
+    scrape_page(skill_href,all_jobs_skill)
+print("all_jobs_skill")
+print(len(all_jobs_skill))

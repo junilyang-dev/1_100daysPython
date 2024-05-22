19e9e90bb29d068adeb8ab2d9687a3904d90481d
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-05-22 13:14:29 +0000

    스크랩 페이지 beautifulsoup

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-import requests
+import requests 
+from bs4 import BeautifulSoup
 
-movie_ids = [
-    238, 680, 550, 185, 641, 515042, 152532, 120467, 872585, 906126, 840430
-]
+all_jobs = []
 
-for movie_id in movie_ids:
-    url = f"https://nomad-movies.nomadcoders.workers.dev/movies/{movie_id}"
-    response = requests.get(url)
-    movie_data = response.json()
-    print(
-        "title : ",movie_data["title"],
-        "\n overview : " ,movie_data["overview"], 
-        "\n vote_average : ",movie_data["vote_average"],
-        "\n -----------------------------------------------"
-        )
+def scrape_page(url):
+  response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
+  soup = BeautifulSoup(response.content, "html.parser",)
+  jobs = soup.find("ul", class_="jobs-list-items").find_all("li")[0:]
+
+  for job in jobs:
+    company = job.find("a", class_="bjs-jlid__b").text
+    title = job.find("h4", class_="bjs-jlid__h").text
+    desc = job.find("div", class_="bjs-jlid__description").text 
+    url = job.find("h4", class_="bjs-jlid__h").find("a")["href"]
+
+    job_data = {
+      'title': title,
+      'company': company,
+      'desc': desc,
+      'url': url
+    }
+    all_jobs.append(job_data)
+
+def get_pages(url):
+  response = requests.get(url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"})
+  soup = BeautifulSoup(response.content, "html.parser",)
+  return len(soup.find("ul", class_="bsj-nav").find_all("a"))
+
+total_pages = get_pages("https://berlinstartupjobs.com/engineering")
+
+for x in range(total_pages-1):
+  url = f"https://berlinstartupjobs.com/engineering/page/{x+1}"
+  scrape_page(url)
+
+
+print(len(all_jobs))
